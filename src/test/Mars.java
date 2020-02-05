26f0bf384a5f817b48cdb951dec97767c490c2a1
ozlemdeveci <56897603+ozlemdeveci@users.noreply.github.com>
2020-02-04 20:05:59 -0600

    Update Mars.java
    changed

--- src/test/Mars.java
+++ src/test/Mars.java
@@ -3,8 +3,14 @@ package test;
 public class Mars {
 
 	public static void main(String[] args) {
-System.out.println("kendini affet");
+         System.out.println("kendini affet");
 
+		
+        System.out.println("you were my best friend");
+		
+		
+		
+		
 	}
 
 }
